65f8a2ef5e7d1cfa199059d3305f49e30b0942f2
bekzodjonsharobiddinov <bekzodjonsharobiddinov@gmail.com>
2024-10-10 12:18:38 +0500

    check react-render 2

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,8 @@ import './i18n';
 import Application from "./Application.tsx";
 import { UserProvider } from "./context/UserContext.tsx";
 import { AppProvider } from "./context/AppContext.tsx";
-import { postEvent } from "@telegram-apps/sdk";
 
 const queryClient = new QueryClient();
-postEvent('web_app_expand');
-postEvent('web_app_setup_main_button', { is_visible: false })
-postEvent('web_app_setup_swipe_behavior', { allow_vertical_swipe: false })
 WebApp.ready();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
